d6588d9827fb849c5ca07c88b0fc38f233f7a5d2
minorusal <minorusal@hotmail.com>
2019-10-08 21:14:47 -0500

    Creacion de registros con TDD

---
+++ tests/features/CreatePostsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+class CreatePostsTest extends FeatureTestCase{
+
+	function test_a_user_create_a_post(){
+		// Having
+		$route = 'posts.create';
+		$title = 'Esta es una pregunta';
+		$content = 'Este es el contenido';
+
+		$this->actingAs($user = $this->defaultUser());
+
+		// When
+		$this->visit(route($route))
+			->type($title, 'title')
+			->type($content, 'content')
+			->press('Publicar');
+
+		// Then	
+		$this->seeInDatabase('posts', [
+			'title' => $title,
+			'content' => $content,
+			'pending' =>true,
+			'user_id' => $user->id,
+		]);
+
+		// Test a user is redirected to the post details after creating it.
+		$this->see($title);
+	}
+}
